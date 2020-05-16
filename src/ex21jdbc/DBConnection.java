229bdffa53a4a625c586bb303e844cf79faace55
alsrb192 <alsrb192@naver.com>
2020-05-16 21:31:45 +0900

    200516 2131

--- src/ex21jdbc/DBConnection.java
+++ src/ex21jdbc/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DBConnection {
-
+//////
 	public static void main(String[] args) 
 			throws ClassNotFoundException, SQLException
 	{
